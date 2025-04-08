7a7192aa8d06f9f596bdde60b58d2ab15902a428
Carmela <carmelaroz@gmail.com>
2025-04-08 19:13:21 +0300

    structure changed

--- b/test.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const Property = require('./models/Property');
-
-console.log('All modules loaded successfully');
